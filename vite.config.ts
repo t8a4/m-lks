2dd86e3fd4dd6f55d2cae53c36e4ffeac2a8acb9
t8a4 <124496669+t8a4@users.noreply.github.com>
2025-08-17 21:53:27 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/m-lks/', // <-- тук сложи ИМЕТО на репото си
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
